8a0866709625be827d906e4abb819367a0f5ba13
JAZUMA <JAZUMA@10.0.0.3>
2024-01-04 18:24:38 -0800

    implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -1,10 +1,15 @@
 package a;
 
 public class DemoA {
-public static void main(String[]args) {
-	System.out.println("Monday");
-	
-	System.out.println("Sprint 1 work");
-	System.out.println("Sprint 2 work");
-}
+	public static void main(String[] args) {
+		System.out.println("Monday");
+
+		System.out.println("Sprint 1 work");
+		System.out.println("Sprint 2 work");
+	}
+
+//Sprint 2 automation
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
 }
