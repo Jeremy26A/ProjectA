901ae9a8c79b5cb308e317084cc15cb67ddf1374
JeremyB11Java <155493139+JeremyB11Java@users.noreply.github.com>
2024-01-04 18:30:20 -0800

    Update DemoA.java

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -16,4 +16,8 @@ public class DemoA {
 	public static void subtract(int a, int b) {
 		System.out.println(a - b);
 	}
+//Coming from github
+	public static void divide(int a, int b) {
+		System.out.println(a / b);
+	}
 }
